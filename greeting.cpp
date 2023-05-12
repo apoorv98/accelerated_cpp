01fae36228eb12244fb45e4f94a9b298386efc1d
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-05-12 12:21:39 +0530

    adding logic for printing individual character

--- greeting.cpp
+++ greeting.cpp
@@ -14,9 +14,11 @@ int main() {
 
   // number of blanks surrounding the greeting
   const int blanks_around_greeting = 1;
+  const int sides_of_greeting = 2;
 
   // total number of rows to write
-  const int total_rows_to_write = blanks_around_greeting * 2 + 3;
+  const int total_rows_to_write =
+      blanks_around_greeting * sides_of_greeting + 3;
 
   // TODO: seperate the output from the input
   std::cout << std::endl;
@@ -27,6 +29,17 @@ int main() {
   // invariant: we have written row_counter rows so far
   while (row_counter != total_rows_to_write) {
     // TODO: write a row of output
+    const std::string::size_type total_columns_to_write =
+        greeting.size() + blanks_around_greeting * sides_of_greeting + 2;
+
+    std::string::size_type column_counter = 0;
+
+    // invariant: we have written column_counter characters so far in the
+    // current row
+    while (column_counter != total_columns_to_write) {
+      // write one or more characters
+      // adjust the value of column_counter to maintain the invariant
+    }
     std::cout << std::endl;
     ++row_counter;
   }
