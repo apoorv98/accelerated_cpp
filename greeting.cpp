f9f827f3a8f0f2b4b29fd47940eb86f5cdad43a2
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-05-12 12:49:24 +0530

    modifying logic and using namespaces

--- greeting.cpp
+++ greeting.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main() {
   // ask for the person's name
-  std::cout << "Please enter your first name: ";
+  cout << "Please enter your first name: ";
 
   // read the name
-  std::string name_to_print;
-  std::cin >> name_to_print;
+  string name_to_print;
+  cin >> name_to_print;
 
   // build the message that we intend to write
-  const std::string greeting = "Hello, " + name_to_print + "!";
+  const string greeting = "Hello, " + name_to_print + "!";
 
   // number of blanks surrounding the greeting
   const int blanks_around_greeting = 1;
   const int sides_of_greeting = 2;
+  const int greeting_and_space_around_it = 3;
 
-  // total number of rows to write
+  // total number of rows and columns to write
   const int total_rows_to_write =
-      blanks_around_greeting * sides_of_greeting + 3;
+      blanks_around_greeting * sides_of_greeting + greeting_and_space_around_it;
+  const string::size_type total_columns_to_write =
+      greeting.size() + blanks_around_greeting * sides_of_greeting + 2;
 
-  // TODO: seperate the output from the input
-  std::cout << std::endl;
+  // write a blank line to seperate the output from the input
+  cout << endl;
 
   // write rows of the output
-  int row_counter = 0;
-
   // invariant: we have written row_counter rows so far
-  while (row_counter != total_rows_to_write) {
-    // TODO: write a row of output
-    const std::string::size_type total_columns_to_write =
-        greeting.size() + blanks_around_greeting * sides_of_greeting + 2;
-
-    std::string::size_type column_counter = 0;
+  for (int row_counter = 0; row_counter != total_rows_to_write; ++row_counter) {
+    string::size_type column_counter = 0;
 
     // invariant: we have written column_counter characters so far in the
     // current row
     while (column_counter != total_columns_to_write) {
-      // write one or more characters
-      // adjust the value of column_counter to maintain the invariant
+      // is it time to write the greeting
+      if (row_counter == blanks_around_greeting + 1 &&
+          column_counter == blanks_around_greeting + 1) {
+        cout << greeting;
+        column_counter += greeting.size();
+      } else {
+        // are we on the border
+        if (row_counter == 0 || row_counter == total_rows_to_write - 1 ||
+            column_counter == 0 || column_counter == total_columns_to_write - 1)
+          cout << "*";
+        else
+          cout << " ";
+        ++column_counter;
+      }
     }
-    std::cout << std::endl;
-    ++row_counter;
+    cout << endl;
   }
-
   return 0;
 }
