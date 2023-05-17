cb70a0849f7ee7db3431df448392d88f702a412b
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-05-17 21:58:53 +0530

    using iter instead of indices

--- grade.cpp
+++ grade.cpp
@@ -31,14 +31,14 @@ bool fgrade(const Student_info &s) { return grade(s) < 60; }
 // separate passing and failing student records
 vector<Student_info> extract_fails(vector<Student_info> &students) {
   vector<Student_info> fail;
-  vector<Student_info>::size_type i = 0;
+  vector<Student_info>::iterator iter = students.begin();
 
-  while (i != students.size()) {
-    if (fgrade(students[i])) {
-      fail.push_back(students[i]);
-      students.erase(students.begin() + i);
+  while (iter != students.end()) {
+    if (fgrade(*iter)) {
+      fail.push_back(*iter);
+      iter = students.erase(iter);
     } else
-      ++i;
+      ++iter;
   }
   return fail;
 }
