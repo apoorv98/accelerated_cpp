018d9159e6f3e6f4141ea3dcc6f7bd6f321d2a33
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-08-12 20:51:40 +0530

    basic Vec implementation

--- Vec.cpp
+++ Vec.cpp
@@ -1,8 +1,13 @@
-template <class T> class Vec {
-public:
-  // interface
+#include "Vec.h"
 
-private:
-  T *data;  // first element in the Vec
-  T *limit; // one past the last element in Vec
-};
+template <class T> Vec<T> &Vec<T>::operator=(const Vec &rhs) {
+  // check for self-assignmen
+  if (&rhs != this) {
+    // free the array in left-hand side
+    uncreate();
+
+    // copy elements from right-hand side to left-hand side
+    create(rhs.begin(), rhs.end());
+  }
+  return *this;
+}
---
+++ Vec.h
@@ -0,0 +1,33 @@
+#ifndef VEC_H_
+#define VEC_H_
+
+template <class T> class Vec {
+public:
+  typedef T *iterator;
+  typedef const T *const_iterator;
+  typedef size_t size_type;
+  typedef T value_type;
+
+  Vec() { create(); }
+  Vec(const Vec &v) { create(v.begin(), v.end()); } // copy constructor
+  explicit Vec(size_type n, const T &val = T()) { create(n, val); }
+  ~Vec() { uncreate(); }
+
+  size_type size() const { return limit - data; }
+
+  T &operator[](size_type i) { return data[i]; }
+  const T &operator[](size_type i) const { return data[i]; }
+  Vec &operator=(const Vec &);
+
+  iterator begin() { return data; }
+  const_iterator begin() const { return data; }
+
+  iterator end() { return limit; }
+  const_iterator end() const { return limit; }
+
+private:
+  iterator *data;  // first element in the Vec
+  iterator *limit; // one past the last element in Vec
+};
+
+#endif // VEC_H_
