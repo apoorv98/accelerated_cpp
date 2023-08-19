234be3ad15885dc336cba92d09ac3caf81e7668a
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-08-19 20:53:27 +0530

    implementation of vcat

--- Picture.cpp
+++ Picture.cpp
@@ -41,3 +41,18 @@ void String_Pic::display(ostream &os, ht_sz row, bool do_pad) const {
   if (do_pad)
     pad(os, start, width());
 }
+
+void VCat_Pic::display(ostream &os, ht_sz row, bool do_pad) const {
+  wd_sz w = 0;
+  if (row < top->height()) {
+    // we are in the top subpicture
+    top->display(os, row, do_pad);
+    w = top->width();
+  } else if (row < height()) {
+    // we are in the bottom subpicture
+    bottom->display(os, row - top->height(), do_pad);
+    w = bottom->width();
+  }
+  if (do_pad)
+    pad(os, w, width());
+}
--- Picture.h
+++ Picture.h
@@ -44,12 +44,13 @@ class Frame_Pic : public Pic_base {
 };
 
 class VCat_Pic : public Pic_base {
+  friend Picture vcat(const Picture &, const Picture &);
   Ptr<Pic_base> top, bottom;
   VCat_Pic(const Ptr<Pic_base> &t, const Ptr<Pic_base> &b)
       : top(t), bottom(b) {}
 
-  wd_sz width() const;
-  ht_sz height() const;
+  wd_sz width() const { return std::max(top->width(), bottom->width()); }
+  ht_sz height() const { return top->height() + bottom->height(); }
   void display(std::ostream &, ht_sz, bool) const;
 };
 
