3cb5ba6c88baa8094405a02a86b1c2d42fdb11b0
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-08-19 20:07:44 +0530

    design of derived classes

--- Picture.h
+++ Picture.h
@@ -16,10 +16,44 @@ class Pic_base {
   virtual void display(std::ostream &, ht_sz, bool) const = 0;
 };
 
-class String_Pic : public Pic_base {};
-class Frame_Pic : public Pic_base {};
-class VCat_Pic : public Pic_base {};
-class HCat_Pic : public Pic_base {};
+class String_Pic : public Pic_base {
+  std::vector<std::string> data;
+  String_Pic(const std::vector<std : string & v>) : data(v) {}
+
+  wd_sz width() const;
+  ht_sz height() const;
+  void display(std::ostream &, ht_sz, bool) const;
+};
+
+class Frame_Pic : public Pic_base {
+  // no public interface
+  Ptr<Pic_base> p;
+  Frame_Pic(const Ptr<Pic_base> &pic) : p(pic) {}
+
+  wd_sz width() const;
+  ht_sz height() const;
+  void display(std::ostream &, ht_sz, bool) const;
+};
+
+class VCat_Pic : public Pic_base {
+  Ptr<Pic_base> top, bottom;
+  VCat_Pic(const Ptr<Pic_base> &t, const Ptr<Pic_base> &b)
+      : top(t), bottom(b) {}
+
+  wd_sz width() const;
+  ht_sz height() const;
+  void display(std::ostream &, ht_sz, bool) const;
+};
+
+class HCat_Pic : public Pic_base {
+  Ptr<Pic_base> left, right;
+  HCat_Pic(const Ptr<Pic_base> &l, const Ptr<Pic_base> &r)
+      : left(l), right(r) {}
+
+  wd_sz width() const;
+  ht_sz height() const;
+  void display(std : ostream &, ht_sz, bool) const;
+};
 
 // public interface class and operations
 class Picture {
