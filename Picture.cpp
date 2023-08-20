ecbec42999df4df2858497c85e370fa515d721af
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-08-20 11:20:53 +0530

    implementation of hcat

--- Picture.cpp
+++ Picture.cpp
@@ -56,3 +56,8 @@ void VCat_Pic::display(ostream &os, ht_sz row, bool do_pad) const {
   if (do_pad)
     pad(os, w, width());
 }
+
+void HCat_Pic::display(ostream &os, ht_sz row, bool do_pad) const {
+  left->display(os, row, do_pad || row < right->height());
+  right->display(os, row, do_pad);
+}
--- Picture.h
+++ Picture.h
@@ -55,12 +55,13 @@ class VCat_Pic : public Pic_base {
 };
 
 class HCat_Pic : public Pic_base {
+  friend Picture hcat(const Picture &, const Picture &);
   Ptr<Pic_base> left, right;
   HCat_Pic(const Ptr<Pic_base> &l, const Ptr<Pic_base> &r)
       : left(l), right(r) {}
 
-  wd_sz width() const;
-  ht_sz height() const;
+  wd_sz width() const { return left->width() + right->width(); }
+  ht_sz height() const { return std::max(left->height(), right->height()); }
   void display(std : ostream &, ht_sz, bool) const;
 };
 
